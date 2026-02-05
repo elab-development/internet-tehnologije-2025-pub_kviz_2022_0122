edde7cf36bd5870eceb3559feb6f6b886f06c1d0
ratkoviclazar <lazarratkovic0@gmail.com>
2026-02-05 16:23:31 +0100

    feat(home-page): Add upcoming events section to home page - Added EventSection component to display upcoming events on the home page - Updated HomeHero component to include the new EventSection - Styled the EventSection to match the overall design of the home page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ export default function ButtonLink({ href, label }: Props) {
   return (
     <Link
       href={href}
-      className="inline-flex text-center w-50 h-12 border-2 border-pub-orange items-center justify-center rounded-lg bg-pub-beige px-4 py-2 text-[1em] font-semibold text-pub-orange transition duration-500 hover:bg-pub-orange hover:text-pub-beige"
+      className="inline-flex text-center w-50 h-12 border-2 border-pub-orange items-center justify-center rounded-lg bg-pub-beige px-4 py-2 text-[1em] font-semibold text-black/80 transition duration-500 hover:bg-pub-orange hover:text-pub-beige"
     >
       {label}
     </Link>
---
+++ src/constants/eventsTableItems.ts
@@ -0,0 +1,8 @@
+export const eventsTableItems = [
+  { key: "name", label: "Naziv" },
+  { key: "theme", label: "Tema" },
+  { key: "location", label: "Lokacija" },
+  { key: "event_date", label: "Datum" },
+  { key: "capacity", label: "Kapacitet" },
+  { key: "action", label: "Akcija" },
+];
