1d75423befccaeca9f6353a1b434344c444bc31d
ratkoviclazar <lazarratkovic0@gmail.com>
2026-02-08 18:48:27 +0100

    feat(info-card): Promenjen design info card komponente i dodat na profil stranicu

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,7 @@
 
 import { useAuth } from "@/components/AuthProvider";
 import Button from "@/components/Button";
+import InfoCard from "@/components/InfoCard";
 
 export default function LogoutButton() {
   const { logout, status, user } = useAuth();
@@ -19,9 +20,13 @@ export default function LogoutButton() {
   if (status === "unauthenticated") window.location.href = "/login";
 
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-pub-blue">
-      {" "}
-      <Button onClick={logout} label="Odjavi se"></Button>
+    <div className="w-full h-screen flex space-x-10 items-center justify-center bg-pub-blue">
+      <InfoCard
+        title={user?.name || "Korisnik"}
+        fields={[{ label: "Email", value: user?.email }]}
+        variant="user-profile"
+        action={<Button onClick={logout} label="Odjavi se"></Button>}
+      />
     </div>
   );
 }
